c17f90c48efee42beff5aad9384347c1b697f117
DipRabadiya27 <diprabadiya@gmail.com>
2023-11-23 09:53:13 +0530

    All Done

--- springrest/src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
+++ springrest/src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
@@ -15,15 +15,11 @@ public class CourseServiceImpl implements CourseService{
 
     @Autowired
     private CourseDao courseDao;
-//    List<Course>  list;
+
 
 
 
     public CourseServiceImpl() {
-//        list = new ArrayList<>();
-//        list.add(new Course(123,"Java Course","This Course for Java DSA"));
-//        list.add(new Course(132,"C++ Course","This Course for C++ DSA"));
-
 
     }
     @Override
@@ -33,31 +29,17 @@ public class CourseServiceImpl implements CourseService{
 
     @Override
     public Course getCourse(long courseId) {
-//        Course c = null;
-//        for(Course course: list) {
-//            if(course.getId() == courseId){
-//                c=course;
-//                break;
-//            }
-//        }
         return courseDao.getOne(courseId);
     }
 
     @Override
     public Course addCourse(Course course) {
-//        list.add(course);
         courseDao.save(course);
         return course;
     }
 
     @Override
     public Course updateCourse(Course course) {
-//        list.forEach(e -> {
-//            if(e.getId() == course.getId()) {
-//                e.setTitle(course.getTitle());
-//                e.setDescription(course.getDescription());
-//            }
-//        });
         courseDao.save(course);
         return course;
     }
@@ -66,6 +48,5 @@ public class CourseServiceImpl implements CourseService{
     public void deleteCourse(long parseLong) {
         Course entity = courseDao.getOne(parseLong);
         courseDao.delete(entity);
-//        list = this.list.stream().filter(e -> e.getId()!= parseLong).collect(Collectors.toList());
     }
 }
